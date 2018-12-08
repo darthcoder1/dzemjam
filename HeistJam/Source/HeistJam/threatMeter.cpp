651cfa2340b787f3f8cf46435f455d0325bb9913
Woj <derjah@googlemail.com>
2018-12-08 19:24:22 +0100

    Fixed replication on ThreatMeter. Added Door and Laser interactables.

--- HeistJam/Source/HeistJam/threatMeter.cpp
+++ HeistJam/Source/HeistJam/threatMeter.cpp
@@ -9,6 +9,7 @@ AthreatMeter::AthreatMeter()
 	threat = 0;
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
 
 }
 
